da92d7b18c1dd985493d1b19e582d44466dc7f79
Nate Close <closen@seas.upenn.edu>
2012-09-12 00:02:12 -0400

    added other files

---
+++ hw1 code closen.py
@@ -0,0 +1,20 @@
+# Nate Close: closen@seas.upenn.edu
+
+# Import the corpus reader
+from nltk.corpus import PlaintextCorpusReader
+
+# lists all sub-directories from this directory
+def get_sub_directories(directory):
+
+# gets all files in this directory and its sub-directories
+def get_all_files(directory):
+    files = PlaintextCorpusReader(directory, '.*')
+    return files.fileids() 
+
+# returns a list of all sentences in that file
+def load_file_sentences(filepath):
+
+# load all sentences in files within this drectory
+# should return list of sentences
+def load_collection_sentences(directory):
+

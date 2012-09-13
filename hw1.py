6ce4bc9cc991e2a0f7a0b7f942bf8383a80f9d00
Nate Close <closen@seas.upenn.edu>
2012-09-13 00:06:07 -0400

    more updates

--- hw1.py
+++ hw1.py
@@ -3,6 +3,7 @@
 # Import the corpus reader
 from nltk.corpus import PlaintextCorpusReader
 from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
 
 # lists all sub-directories from this directory
 def get_sub_directories(directory):
@@ -16,9 +17,7 @@ def get_all_files(directory):
 # returns a list of all sentences in that file
 def load_file_sentences(filepath):
     file1 = open(filepath)
-    sent = ''
-    for line in file1:
-        sent = sent + line
+    sent = file1.read()
     sent = sent.lower()
     return sent_tokenize(sent)
 
@@ -30,5 +29,22 @@ def load_collection_sentences(directory):
     for f in files:
         sents = load_file_sentences(directory + "/" + f)
         li.extend(sents)
+    return li
+
+# returns a list of all tokens in a file
+def load_file_tokens(filepath):
+    file1 = open(filepath)
+    text = file1.read()
+    text = text.lower()
+    return word_tokenize(text)
+
+# load all tokens in files within this directory
+# should return list of tokens
+def load_collection_tokens(directory):
+    files = get_all_files(directory)
+    li = list()
+    for f in files:
+        tokens = load_file_tokens(directory + "/" + f)
+        li.extend(tokens)
     return li
 

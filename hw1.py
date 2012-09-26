972a82c7b19be046af0280ba4876037fdfe9eac0
Jason S. Mow <jason.mow@gmail.com>
2012-09-25 20:33:53 -0400

    switch back to small ts file

--- hw1.py
+++ hw1.py
@@ -269,7 +269,7 @@ def get_common_contexts(word1, word2, path):
     return list(set(l1) & set(l2))
 
 def compare_word_sim(path):
-    tw = load_topic_words('Starbucks_full.ts')
+    tw = load_topic_words('Starbucks_small.ts')
     top_words = get_top_n_topic_words(tw, 10)
     contexts = list()
 
